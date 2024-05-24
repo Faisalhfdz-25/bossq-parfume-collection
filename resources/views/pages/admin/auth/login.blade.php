7360b37203e766bce4c2151873e83855b34b6672
Faisalhfdz-25 <sendipede093@gmail.com>
2024-05-24 09:32:00 +0700

    update: bg-login

--- resources/views/pages/admin/auth/login.blade.php
+++ resources/views/pages/admin/auth/login.blade.php
@@ -3,28 +3,38 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
-        name="viewport">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Login &mdash; Bossq Parfume Collection</title>
 
     <!-- General CSS Files -->
-    <link rel="stylesheet"
-        href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    <link rel="stylesheet" href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer" />
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- CSS Libraries -->
-    <link rel="stylesheet"
-        href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
 
     <!-- Template CSS -->
-    <link rel="stylesheet"
-        href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+    <style>
+        .background-walk-y {
+            background-image: url("{{ asset('img/unsplash/pp.png') }}");
+            background-size: cover;
+            background-position: center center;
+            background-repeat: no-repeat;
+            
+        }
+    
+        @media (max-width: 768px) {
+            .background-walk-y {
+                background-size: contain; /* Atau bisa menggunakan contain, tergantung kebutuhan */
+                background-position: top center; /* Atau sesuai kebutuhan */
+                background-color: rgb(26, 29, 46); /* Warna background untuk mobile dengan transparansi */
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -33,94 +43,62 @@
             <div class="d-flex align-items-stretch flex-wrap">
                 <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
                     <div class="m-3 p-4">
-                        <img src="{{ asset('img/bossq-parfume-logo.svg') }}"
-                            alt="logo"
-                            width="200"
-                            height="180"
+                        <img src="{{ asset('img/bp-logo-2.svg') }}" alt="logo" width="200" height="200"
                             class="shadow-light rounded-circle mb-5 mt-2">
-                        <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">BossQ Parfume Admin</span>
+                        <h4 class="text-dark font-weight-normal">Selamat datang! <span class="font-weight-bold">BossQ
+                                Parfume Admin</span>
                         </h4>
-                        <p class="text-muted">Before you get started, you must login or register if you don't already
-                            have an account.</p>
-                        <form method="POST"
-                            action="{{route('login')}}"
-                            class="needs-validation"
-                            novalidate="">
+                        <p class="text-muted">Sebelum lanjut bekerja, silahkan untuk Login terlebih dahulu!</p>
+                        <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                             @csrf
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input id="email"
-                                    type="email"
-                                    class="form-control"
-                                    name="email"
-                                    tabindex="1"
-                                    required
-                                    autofocus>
-                                <div class="invalid-feedback">
-                                    Please fill in your email
-                                </div>
+                                <input id="email" type="email"
+                                    class="form-control
+                                    @error('email')
+                                        is-invalid
+                                    @enderror"
+                                    name="email" tabindex="1" required autofocus>
+                                @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+
                             </div>
 
                             <div class="form-group">
                                 <div class="d-block">
-                                    <label for="password"
-                                        class="control-label">Password</label>
+                                    <label for="password" class="control-label">Password</label>
                                 </div>
-                                <input id="password"
-                                    type="password"
-                                    class="form-control"
-                                    name="password"
-                                    tabindex="2"
-                                    required>
-                                <div class="invalid-feedback">
-                                    please fill in your password
-                                </div>
+                                <input id="password" type="password"
+                                    class="form-control @error('password')
+                                        is-invalid
+                                    @enderror"
+                                    name="password" tabindex="2" required>
+                                @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
-
-                            <div class="form-group">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox"
-                                        name="remember"
-                                        class="custom-control-input"
-                                        tabindex="3"
-                                        id="remember-me">
-                                    <label class="custom-control-label"
-                                        for="remember-me">Remember Me</label>
-                                </div>
-                            </div>
-
                             <div class="form-group text-right">
-                                <a href="auth-forgot-password.html"
-                                    class="float-left mt-3">
-                                    Forgot Password?
-                                </a>
-                                <button type="submit"
-                                    class="btn btn-primary btn-lg btn-icon icon-right"
+                                <button type="submit" class="btn btn-primary btn-lg btn-icon icon-right"
                                     tabindex="4">
                                     Login
                                 </button>
                             </div>
-
-                           
                         </form>
-
-                       >
                     </div>
                 </div>
                 <div class="col-lg-8 col-12 order-lg-2 min-vh-100 background-walk-y position-relative overlay-gradient-bottom order-1"
-                    data-background="{{ asset('img/unsplash/login-bg.jpg') }}">
+                    data-background="{{ asset('img/unsplash/pp.png') }}">
                     <div class="absolute-bottom-left index-2">
                         <div class="text-light p-5 pb-2">
                             <div class="mb-5 pb-3">
-                                <h1 class="display-4 font-weight-bold mb-2">Good Morning</h1>
-                                <h5 class="font-weight-normal text-muted-transparent">Bali, Indonesia</h5>
+                                <h1 class="display-4 font-weight-bold mb-2"><span id="greeting-message"></span></h1>
+                                <h5 class="font-weight-normal text-muted-transparent">Bandung, Indonesia</h5>
                             </div>
-                            Photo by <a class="text-light bb"
-                                target="_blank"
-                                href="https://unsplash.com/photos/a8lTjWJJgLA">Justin Kauffman</a> on <a
-                                class="text-light bb"
-                                target="_blank"
-                                href="https://unsplash.com">Unsplash</a>
                         </div>
                     </div>
                 </div>
@@ -144,6 +122,30 @@
     <!-- Template JS File -->
     <script src="{{ asset('js/scripts.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
+    <script>
+        // Fungsi untuk mengubah pesan selamat berdasarkan waktu saat ini
+        function updateGreeting() {
+            var currentTime = new Date().getHours();
+            var greeting;
+
+            if (currentTime >= 5 && currentTime < 12) {
+                greeting = 'Selamat pagi';
+            } else if (currentTime >= 12 && currentTime < 18) {
+                greeting = 'Selamat siang';
+            } else {
+                greeting = 'Selamat malam';
+            }
+
+            // Ubah teks pesan selamat
+            document.getElementById('greeting-message').innerText = greeting;
+        }
+
+        // Panggil fungsi untuk pertama kali saat halaman dimuat
+        updateGreeting();
+
+        // Fungsi untuk memperbarui pesan selamat setiap menit
+        setInterval(updateGreeting, 60000); // 60000 milidetik = 1 menit
+    </script>
 </body>
 
 </html>
