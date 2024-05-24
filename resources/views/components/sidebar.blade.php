459eeddc02a795363d8196bd314b5d38c750bd97
Faisalhfdz-25 <sendipede093@gmail.com>
2024-05-24 07:19:53 +0700

    update: sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -9,126 +9,69 @@
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             {{-- <li class="nav-item dropdown {{ $type_menu === 'dashboard' ? 'active' : '' }}"> --}}
-                <li class="nav-item dropdown">
-                <a href=""
-                    class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            <li class="nav-item dropdown">
+                <a href="" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
                     <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
-                        <a class="nav-link"
-                            href="{{ route('home') }}">Ecommerce Dashboard</a>
+                        <a class="nav-link" href="{{ route('home') }}">Ecommerce Dashboard</a>
                     </li>
                 </ul>
             </li>
             <li class="menu-header">Master</li>
             {{-- <li class="nav-item dropdown {{ $type_menu === 'layout' ? 'active' : '' }}"> --}}
-                <li class="nav-item dropdown">
-                <a href="#"
-                    class="nav-link has-dropdown"
-                    data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Data Master</span></a>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
+                        class=" fas fa-regular fa-folder-open"></i></i> <span>Data Master</span></a>
                 <ul class="dropdown-menu">
                     <li class="{{ Request::is('products') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ route('products.index') }}">Product</a>
+                        <a class="nav-link" href="{{ route('products.index') }}"><i
+                                class=" fas fa-regular fa-bag-shopping"></i></i>Product</a>
                     </li>
                     <li class="{{ Request::is('categories') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ route('categories.index') }}">Category</a>
+                        <a class="nav-link" href="{{ route('categories.index') }}"><i
+                                class=" fas fa-solid fa-tags"></i>Category</a>
                     </li>
-                    {{-- <li class="{{ Request::is('product-galleries') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ route('product-galleries.index') }}">Galleri</a>
-                    </li> --}}
+
                     <li class="{{ Request::is('suppliers') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ route('suppliers.index') }}">Suppliers</a>
+                        <a class="nav-link" href="{{ route('suppliers.index') }}"><i
+                                class="fas fa-solid fa-users-line"></i>Suppliers</a>
                     </li>
                     <li class="{{ Request::is('user*') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ route('user.index') }}">User</a>
+                        <a class="nav-link" href="{{ route('user.index') }}"><i
+                                class="fas fa-regular fa-address-card"></i>User</a>
                     </li>
-                    
-                    
-                    
-                    
+
+
+
+
                 </ul>
             </li>
             </li>
+            <hr width="80%">
             <li class="menu-header">Transaksi</li>
             {{-- <li class="nav-item dropdown {{ $type_menu === 'layout' ? 'active' : '' }}"> --}}
-                <li class="nav-item dropdown">
-                <a href="#"
-                    class="nav-link has-dropdown"
-                    data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Data Transaksi</span></a>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-solid fa-cash-register"></i></i>
+                    <span>Data Transaksi</span></a>
                 <ul class="dropdown-menu">
                     <li class="{{ Request::is('layout-default-layout') ? 'active' : '' }}">
-                        <a class="nav-link"
-                            href="{{ url('layout-default-layout') }}">Trasaksi</a>
+                        <a class="nav-link" href="{{ url('layout-default-layout') }}"><i
+                                class="fas fa-solid fa-hand-holding-dollar"></i>Trasaksi</a>
                     </li>
-                    <li class="{{ Request::is('transparent-sidebar') ? 'active' : '' }}">
-                        <a class="nav-link"
-                            href="{{ url('transparent-sidebar') }}">Category</a>
-                    </li>
+
                 </ul>
             </li>
             </li>
+            <hr width="80%">
             <li class="menu-header">Laporan</li>
-                    {{-- <li class="nav-item dropdown {{ $type_menu === 'layout' ? 'active' : '' }}"> --}}
-                        <li class="nav-item dropdown">
-                        <a href="#"
-                            class="nav-link has-dropdown"
-                            data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Data Laporan</span></a>
-                        <ul class="dropdown-menu">
-                            <li class="{{ Request::is('layout-default-layout') ? 'active' : '' }}">
-                                <a class="nav-link"
-                                    href="{{ url('layout-default-layout') }}">Trasaksi</a>
-                            </li>
-                            <li class="{{ Request::is('transparent-sidebar') ? 'active' : '' }}">
-                                <a class="nav-link"
-                                    href="{{ url('transparent-sidebar') }}">Category</a>
-                            </li>
-
-
-
-
-
-
-           {{-- <li class="menu-header">Landing Pages</li> --}}
-           
-                {{-- <li class="nav-item dropdown"> --}}
-                {{-- <a href="#"
-                    class="nav-link has-dropdown"><i class="fas fa-th-large"></i>
-                    <span>Components</span></a> --}}
-                {{-- <ul class="dropdown-menu"> --}}
-                    {{-- <li class="{{ Request::is('landing-pages') ? 'active' : '' }}">
-                        <a class="nav-link"
-                            href="{{ route('landing_pages.index') }}">Main Content</a>
-                    </li> --}}
-                    {{-- <li class="{{ Request::is('components-avatar') ? 'active' : '' }}">
-                        <a class="nav-link beep beep-sidebar"
-                            href="{{ url('components-avatar') }}">Content</a>
-                    </li> --}}
-                    {{-- <li class="{{ Request::is('components-chat-box') ? 'active' : '' }}">
-                        <a class="nav-link"
-                            href="{{ url('components-chat-box') }}">Banner</a>
-                    </li> --}}
-                    {{-- <li class="{{ Request::is('components-empty-state') ? 'active' : '' }}">
-                        <a class="nav-link beep beep-sidebar"
-                            href="{{ url('components-empty-state') }}">Single Photo</a>
+            {{-- <li class="nav-item dropdown {{ $type_menu === 'layout' ? 'active' : '' }}"> --}}
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fa-solid fa-file-lines"></i></i>
+                    <span>Data Laporan</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{ Request::is('layout-default-layout') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ url('layout-default-layout') }}">Trasaksi</a>
                     </li>
-                    <li class="{{ Request::is('components-gallery') ? 'active' : '' }}">
-                        <a class="nav-link"
-                            href="{{ url('components-gallery') }}">Gallery</a>
-                    </li>
-                    <li class="{{ Request::is('components-hero') ? 'active' : '' }}">
-                        <a class="nav-link beep beep-sidebar"
-                            href="{{ url('components-hero') }}">Hero</a> --}}
-                    {{-- </li>
-                </ul> --}}
-            {{-- </li> --}}
-          
-            
-            
-            
-            
-            
-           
-           
-        </ul>
-
+                </ul>
     </aside>
 </div>
